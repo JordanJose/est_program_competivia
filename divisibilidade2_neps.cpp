bfd1b6535c8697d42ec2f2ff82e31872bc4fe82a
Jose Jordan <jordanjose0410@gmail.com>
2021-05-28 09:13:45 -0400

    Resolução da questão 5

---
+++ divisibilidade2_neps.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int calcula_resto(string numero, int m){
+    int resto = 0, pot10;
+    for(int i = 0; i < numero.size(); i++){
+        pot10 = ((10%m * resto%m)%m);
+        resto = (pot10%m + (numero[i]-'0')%m)%m;
+    }
+    return resto;
+}
+
+int main(){
+    string numero;
+    cin >> numero;
+    if(!calcula_resto(numero, 4)){
+        cout << "S" << endl;
+    } else{
+        cout << "N" << endl;
+    }
+    if(!calcula_resto(numero, 9)){
+        cout << "S" << endl;
+    } else{
+        cout << "N" << endl;
+    }
+    if(!calcula_resto(numero, 25)){
+        cout << "S" << endl;
+    } else{
+        cout << "N" << endl;
+    }
+}
