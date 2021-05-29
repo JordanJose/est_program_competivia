5f29c39fac3a5429e47ebe4240ead73cfd14373d
Jose Jordan <jordanjose0410@gmail.com>
2021-05-28 20:41:03 -0400

    Organizando

--- abc_uri_2336.cpp
+++ abc_uri_2336.cpp
@@ -2,12 +2,11 @@
 
 using namespace std;
 
-#define M ((int)1e9 + 7);
+#define MOD (int)(1e9 + 7)
 
 int main(){
     string abc;
     unsigned long long int resto = 0, pot26;
-    long long int MOD = M;
     while(cin >> abc){
         resto = 0;
         for(int i = 0; i < abc.size(); i++){
